f0ef436f4642ed5cfb8ce0b50248257e0a4bc2c4
15105090 <15105090@facin.portoalegre.pucrsnet.br>
2019-04-12 18:54:33 -0300

    primeiro passo

--- Peer.java
+++ Peer.java
@@ -1,6 +1,7 @@
 // Bibliotecas de estabelecimento de conexao do Java
-import java.net.Socket;
-import java.net.ServerSocket;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
 
 public class Peer{
 	
@@ -10,6 +11,52 @@ public class Peer{
 	// Cria o Peer
 	public Peer(String ip){
 		this.ip = ip;
+		start();
+	}
+	
+	private String getIp(){
+		return ip;
+	}
+	
+	// Bases for new methods
+	// Starts an user interface. If he get disconnected, comes back here
+	private void start(){
+	}
+	
+	// Logs on a Super Node
+	private void login(){
+	}
+	
+	// Thread that sends "Im here"
+	private void stillAlive(){
+	}
+	
+	// Requests a file from another Peer to the Supernode
+	private void requestFile(){
+	}
+	
+	// Receives response from SuperNode
+	private void getRequest(){
+	}
+	
+	// Listens to Supernode
+	private void listen(){
+	}
+	
+	// Send answer to Supernode
+	private void answer(){
+	}
+	
+	// Gets file from another node
+	private void getFile(){
+	}
+	
+	// Transmits file to another node
+	private void transmitFile(){
+	}
+	
+	// Logout from current supernode
+	private void logout(){
 	}
 	
 }
--- Supernode.java
+++ Supernode.java
@@ -3,8 +3,9 @@ import java.util.ArrayList;
 import java.io.IOException;
 
 // Bibliotecas de estabelecimento de conexao do Java
-import java.net.Socket;
-import java.net.ServerSocket;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.MulticastSocket;
 
 public class Supernode {
@@ -12,12 +13,42 @@ public class Supernode {
 	// Super nodo tera seu IP e lista de peers vinculados a ele
 	private String ip;
 	private ArrayList<Peer> Nodes;
-	private MulticastSocket socket;
 	
 	// Cria o Super nodo
-	public Supernode(String ip) throws IOException {
+	public Supernode(String ip) {
 		this.ip = ip;
 		this.Nodes = new ArrayList<>();
-		socket = new MulticastSocket(6969);
+		start();
+	}
+	
+	private String getIp(){
+		return ip;
+	}
+	
+	// Bases for new methods
+	// Starts an user interface. If he get disconnected, comes back here
+	private void start(){
+	}
+	
+	// Log peer
+	
+	// Thread that receives the "Im here"
+	private void getPeer(){
+	}
+	
+	// Receive Multicast
+	
+	// Send back Multicast to supernode
+	
+	// Receive peer
+	
+	// Send Multicast
+	
+	// Get Response
+	
+	// Responds to Peer
+	
+	// Disable the entire system
+	private void logout(){
 	}
 }
