ab500f06f1088f5966aae23b55f50fe03af20ed9
15105090 <15105090@facin.portoalegre.pucrsnet.br>
2019-04-15 16:32:05 -0300

    test

--- peer.py
+++ peer.py
@@ -1,4 +1,4 @@
-import socket
+import socket, hashlib
 import _thread as thread
 
 from time import sleep
@@ -6,6 +6,7 @@ from time import sleep
 ip = 'localhost'
 superIp = 'loopback'
 fileName = 'file'
+hashName = 'hash'
 
 #groups and ports
 SUPNODE_PORT = 5003
@@ -25,18 +26,20 @@ def peer(sIp,fileN,pIp):
     thread.start_new_thread(joinSuperNode, (sock,))
     # Agora que ele mandou pro SUPERNODE, espera a resposta dele
     data, address = sock.recvfrom(1024).strip('b')[1:-1]
-    print('JOINED SUPERNODE: ' + data)
-    if data = 'ALLOWED':
+    if data == 'ALLOWED':
+        print('JOINED SUPERNODE: ' + data)
         # AQUI ELE CONFIRMA SE ELE JOINOU OU NAO. Se confirmou, cria Thread do IM ALIVE
         # Nota-se que se o supernodo arrancar ele fora da lista, a interface continua intacta
         # mas qlqr tentativa de conexao vai dar em merda
         
         ''' CRIAR THREAD QUE HASHEIA O FILE DO PEER '''
+        thread.start_new_thread(setHash,())
         ''' CRIAR THREAD QUE MANDA AS INFOS DA PORRA DO PEER PRO SUPERNODE '''
         
         thread.start_new_thread(stillAlive, (sock,))
         
         options()
+        sys.exit()
         #Aqui agora ele tem que criar Thread que escuta um Supernodo pra estabelecer conexao com outro Peer
         
         #Aqui agora tem que criar o bagulho de pedir pra um supernodo um arquivo
@@ -44,6 +47,9 @@ def peer(sIp,fileN,pIp):
     
 def options():
     return
+
+def setHash():
+	hashName = hashlib.md5(fileName.encode('utf-8')).hexdigest()
 
 def joinSuperNode(sock):
     server_addr = (superIp,SUPNODE_PORT)
--- supernode.py
+++ supernode.py
@@ -1,6 +1,8 @@
 import socket, struct
 import _thread as thread
 
+from time import sleep
+
 ip = 'localhost'
 
 #peers (file,hash,peer ip)
@@ -20,22 +22,33 @@ def supernode(superIp):
     
     MCAST_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     MCAST_sock.bind(('',MCAST_PORT))
-    group = MCAST_sock.inet_aton(MCAST_GRP)
+    group = socket.inet_aton(MCAST_GRP)
     mreq = struct.pack('4sL', group, socket.INADDR_ANY)
     MCAST_sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
     
+    #OK ----> CRIA SUPER NODO E ENTRA NO GRUPO DETERMINADO
+    
     # now that ele esta baindado no Multicast lixo do caralho, hora de ver as opcoes
-    thread.start_new_thread(peerJoin, (,))
+    thread.start_new_thread(peerJoin, ())
+    while True:
+        print('2423')
+        sleep(2)
     
     # now that he joined, send the fucking listening options
 
 def peerJoin():
+	# provavelmente while true pra se repetir com Peers
     UNI_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     UNI_sock.bind(('',SUPNODE_PORT))
+    print('quer ver')
     data, address = UNI_sock.recvfrom(1024).strip('b')[1:-1]
     if data == 'LET ME JOIN':
+        signal = bytes(ALLOWED_JOIN,'utf-8')
+        UNI_sock.sendto(signal,address)
         # aqui eu deveria adicionar as merdas do peer
+        print('passei weee')
         thread.start_new_thread(stillAlive, (UNI_sock,))
+        
     
 def stillAlive():
     count = 2
@@ -44,3 +57,4 @@ def stillAlive():
         if data == 'STILL ALIVE':
             sleep(5)
             # Need to perfect this crap
+    UNI_sock.close()
